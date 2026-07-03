03d51285f643a219f5a64a9569c968f319789c58
Brandon Gatewood <brandongatewood9@gmail.com>
2026-07-03 11:06:59 -0700

    added gitignore

--- Services/OrderService/OrderService.Api/obj/Debug/net10.0/OrderService.Api.AssemblyInfo.cs
+++ Services/OrderService/OrderService.Api/obj/Debug/net10.0/OrderService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01968b7c51c30d120d4cc0bc1aa1399e41a85123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3de7a8a6d6beaaf395a4493078c48e297342a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
