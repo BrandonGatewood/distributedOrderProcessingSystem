0c831f8b86b6db92e250c0f811b985fdbf5ee2d9
Brandon Gatewood <brandongatewood9@gmail.com>
2026-07-08 20:16:26 -0700

    Renamed variable and added async processing CreateOrder method now talks to database

--- Services/OrderService/OrderService.Api/Controllers/OrderControllers.cs
+++ Services/OrderService/OrderService.Api/Controllers/OrderControllers.cs
@@ -1,18 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using OrderService.Application.DTOs.Requests;
+using OrderService.Application.DTOs.Responses;
 using OrderService.Application.Interfaces;
 namespace OrderService.Api.Controllers;
 
 [ApiController]
 [Route("api/order")]
-public class OrderControllers(IOrderApplicationService orderService) : ControllerBase
+public class OrderControllers(IOrderApplicationService orderApplicationService) : ControllerBase
 {
-    private readonly IOrderApplicationService _orderService = orderService;
+    private readonly IOrderApplicationService _orderService = orderApplicationService;
 
     [HttpPost]
     public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request)
     {
 
-        return Ok(_orderService.CreateOrderAsync(request));
+        return Ok(await _orderService.CreateOrderAsync(request));
     }
 }
