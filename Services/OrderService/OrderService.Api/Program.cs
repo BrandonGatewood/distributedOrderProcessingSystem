5e7aa29dc0419c7aae48479c6021f2cb96fcf899
Brandon Gatewood <brandongatewood9@gmail.com>
2026-07-08 20:15:58 -0700

    connected to supabase

--- Services/OrderService/OrderService.Api/Program.cs
+++ Services/OrderService/OrderService.Api/Program.cs
@@ -1,16 +1,33 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
 using OrderService.Application.Interfaces;
 using OrderService.Application.Services;
+using OrderService.Infrastructure.Data;
+using OrderService.Infrastructure.Repositories;
+
+
+Env.Load("../../../.env");
 
 var builder = WebApplication.CreateBuilder(args);
 
-if(builder.Environment.IsDevelopment())
-{
-    //Env.load();
-}
+builder.Configuration.AddEnvironmentVariables();
+
+// Db Configuration
+var connectionString = builder.Configuration["ORDERSERVICEDB:CONNECTIONSTRING"]
+    ?? throw new InvalidOperationException("Order Service Database connection string is missing in configuration.");
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IOrderApplicationService, OrderApplicationService>();
+builder.Services.AddDbContext<OrderDbContext>(options =>
+{
+    options.UseNpgsql(connectionString, npgsqlOptions =>
+    {
+        npgsqlOptions.EnableRetryOnFailure(3);
+    });
+});
+
 
 var app = builder.Build();
 app.MapControllers();
