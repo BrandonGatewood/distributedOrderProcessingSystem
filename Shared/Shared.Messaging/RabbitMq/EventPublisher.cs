ad5bfb9b3585e632093164d05ed9a0f20311a181
Brandon Gatewood <brandongatewood9@gmail.com>
2026-09-21 15:21:16 -0700

    Added logging for failed publishing attempts.

--- Shared/Shared.Messaging/RabbitMq/EventPublisher.cs
+++ Shared/Shared.Messaging/RabbitMq/EventPublisher.cs
@@ -1,36 +1,51 @@
 using System.Text;
 using System.Text.Json;
 using Shared.Messaging.Interfaces;
-using Shared.Messaging.Constants;
 using RabbitMQ.Client;
+using Microsoft.Extensions.Logging;
 
 namespace Shared.Messaging.RabbitMq;
 
-public class EventPublisher(IRabbitMqConnection connection) : IEventPublisher
+public class EventPublisher(ILogger<EventPublisher> logger, IRabbitMqConnection connection) : IEventPublisher
 {
+    private readonly ILogger<EventPublisher> _logger = logger;
     private readonly IRabbitMqConnection _connection = connection;
 
     public async Task PublishAsync<T>(string exchange, string routingKey, T message)
     {
-        // Create channel
-        await using var channel = await _connection.Connection.CreateChannelAsync();
+        try
+        {
+            // Create channel
+            await using var channel = await _connection.Connection.CreateChannelAsync();
 
-        // Declare message exchange
-        await channel.ExchangeDeclareAsync(
-            exchange: exchange,
-            type: ExchangeType.Topic,
-            durable: true
-        );
+            // Declare message exchange
+            await channel.ExchangeDeclareAsync(
+                exchange: exchange,
+                type: ExchangeType.Topic,
+                durable: true
+            );
 
-        var json = JsonSerializer.Serialize(message);
+            var json = JsonSerializer.Serialize(message);
 
-        var body = Encoding.UTF8.GetBytes(json);
+            var body = Encoding.UTF8.GetBytes(json);
 
-        // Publish the message
-        await channel.BasicPublishAsync(
-            exchange: exchange,
-            routingKey: routingKey,
-            body: body
-        );
+            // Publish the message
+            await channel.BasicPublishAsync(
+                exchange: exchange,
+                routingKey: routingKey,
+                body: body
+            );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(
+                ex,
+                "Failed to publish event to exchange {Exchange} with routing key {RoutingKey}",
+                exchange,
+                routingKey
+            );
+
+            throw;
+        }
     }
 }
